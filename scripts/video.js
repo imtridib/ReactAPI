7f3290501730b94b2f00e50aba255db79175feb0
Tridib Chakraborty <tridib.ewu.bd@gmail.com>
2025-08-25 01:32:17 +0600

    New update

--- scripts/video.js
+++ scripts/video.js
@@ -45,19 +45,16 @@ const displayVideos = (videos) => {
   videos.forEach((video) => {
     console.log(video);
     const card = document.createElement("div");
-    card.classList = "card bg-base-100";
+    card.classList = "card card-compact ";
     card.innerHTML = `
     <figure class="h-[200px] relative">
      <img
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
-      ${
-        video.others.posted_date?.length == 0
-          ? ""
-          : `<span class='absolute bottom-2 right-2 bg-black text-white py-1>${video.others.posted_date}</span>`
-      }
-      
+      <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${
+        video.others.posted_date
+      }</span>
     </figure>
     <div class="px-0 py-2 flex gap-2">
        <div>
@@ -68,8 +65,14 @@ const displayVideos = (videos) => {
        <div>
           <h2 class="font-bold">${video.title}</h2>
        <div class="flex gap-2 items-center">
-          <p class="text-gray-400">${video.authors[0].profile_name}</p> 
-          
+          <p class="text-gray-400">${video.authors[0].profile_name}</p>
+          ${
+            video.authors[0].verified == true
+              ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"`
+              : ""
+          }
+       </div>   
+       <p> </p>
        </div>
     </div>
     `;
