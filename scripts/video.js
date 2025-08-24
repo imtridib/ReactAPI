b6f7f7e540cde818e00a4f3b3b9490df60eadc5c
Tridib Chakraborty <tridib.ewu.bd@gmail.com>
2025-08-24 21:34:42 +0600

    Initial commit

---
+++ scripts/video.js
@@ -0,0 +1,97 @@
+//1 - Fetch, Load and Show Categories on html
+
+//create loadCategories
+const loadCategories = () => {
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+const loadVideos = () => {
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+// videDemo = {
+//     authors
+// :
+// [{…}]
+// category_id
+// :
+// "1001"
+// description
+// :
+// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// others
+// :
+// {views: '100K', posted_date: '16278'}
+// thumbnail
+// :
+// "https://i.ibb.co/L1b6xSq/shape.jpg"
+// title
+// :
+// "Shape of You"
+// video_id
+// :
+// "aaaa"
+// }
+
+const displayVideos = (videos) => {
+  const videosContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card bg-base-100";
+    card.innerHTML = `
+    <figure class="h-[200px] relative">
+     <img
+      src=${video.thumbnail}
+      class="h-full w-full object-cover"
+      alt="Shoes" />
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class='absolute bottom-2 right-2 bg-black text-white py-1>${video.others.posted_date}</span>`
+      }
+      
+    </figure>
+    <div class="px-0 py-2 flex gap-2">
+       <div>
+            <img class="w-10 h-10 rounded-full object-cover" src=${
+              video.authors[0].profile_picture
+            } />
+       </div>
+       <div>
+          <h2 class="font-bold">${video.title}</h2>
+       <div class="flex gap-2 items-center">
+          <p class="text-gray-400">${video.authors[0].profile_name}</p> 
+          
+       </div>
+    </div>
+    `;
+    videosContainer.append(card);
+  });
+};
+//create Display Categories
+const displayCategories = (categories) => {
+  const categoriesContainer = document.getElementById("categories");
+
+  categories.forEach((item) => {
+    console.log(item);
+    //create a button
+
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    //add button to category container
+    categoriesContainer.appendChild(button);
+  });
+};
+
+loadCategories();
+loadVideos();
